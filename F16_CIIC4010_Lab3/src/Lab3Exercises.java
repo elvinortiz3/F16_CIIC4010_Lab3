1d7087ef565f34bc7cb05659c97c31570cad2065
4015b12 <4015b12@parcha.ece.uprm.edu>
2016-08-31 10:38:04 -0400

    Changed Name and centered 2.0

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -2,9 +2,9 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Elvin Ortiz");
+		JFrame myFrame = new JFrame("Elvin_Ortiz");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(900, 400);
+		myFrame.setLocation(900, 425);
 		myFrame.setSize(200,200);
 		myFrame.setVisible(true);
 	}
